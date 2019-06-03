4090b2148cea052baad95f5ddc766951e092aa10
spnsathis88 <spnsathis88@gmail.com>
2019-06-03 20:04:46 +0530

    Dns Azure updated

---
+++ Content/Networking/DNS/AWS/Route53/WorkloadManager/src/awsroute53/prerequiste_environments.py
@@ -0,0 +1,119 @@
+"This method is used to get the details from user and assigning to domain creation json"
+
+import json,os
+import re,sys
+from util import print_error,print_log,print_result,write_error
+
+try:
+
+    currentworkingdir = os.getcwd()
+    filename = os.path.join(currentworkingdir, "createrecordset.json")
+    print("filename is :",filename)
+    with open(filename, 'r') as file:
+         json_data = json.load(file)
+         global json_data
+         print(json_data)
+
+except IOError as ioerr:
+    print_error(ioerr)
+    write_error(ioerr)
+    sys.exit(127)
+
+try:
+    #Getting inputs from user for creaating sub domain and health check from environment variable.
+    domainName = os.environ["DomainName"]
+    subDomainName = os.environ["subDomainName"]
+    dependents = os.environ.get('CliqrDependencies', "")
+
+    if os.environ["IpAddress"]:
+        IpAddress = os.environ["IpAddress"]
+    else:
+        if len(dependents) > 0:
+            IpAddress = str(os.environ['CliqrTier_' + dependents + '_PUBLIC_IP'])
+            print("my instances Ip address is : ===================>{}".format(IpAddress))
+
+
+    healthCheckport = os.environ["healthCheckport"]
+    healthCheckpath = os.environ["healthCheckpath"]
+
+except Exception as er:
+    print_error("Required parameter is missing .")
+    print_error(er)
+    sys.exit(127)
+
+def create_domain_json():
+
+    json_data["AdminContact"]["FirstName"] = ""
+    json_data["AdminContact"]["LastName"] = ""
+    json_data["AdminContact"]["ContactType"] = ""
+    json_data["AdminContact"]["OrganizationName"] = ""
+    json_data["AdminContact"]["AddressLine1"] = ""
+    json_data["AdminContact"]["AddressLine2"] = ""
+    json_data["AdminContact"]["City"] = ""
+    json_data["AdminContact"]["PhoneNumber"] = ""
+    json_data["AdminContact"]["State"] = ""
+    json_data["AdminContact"]["CountryCode"] = ""
+    json_data["AdminContact"]["ZipCode"] = ""
+    json_data["AdminContact"]["Email"] = ""
+    # json_data["AdminContact"]["FAX"] = ""
+
+
+
+    json_data["RegistrantContact"]["FirstName"] = ""
+    json_data["RegistrantContact"]["LastName"] = ""
+    json_data["RegistrantContact"]["ContactType"] = ""
+    json_data["RegistrantContact"]["OrganizationName"] = ""
+    json_data["RegistrantContact"]["AddressLine1"] = ""
+    json_data["RegistrantContact"]["AddressLine2"] = ""
+    json_data["RegistrantContact"]["City"] = ""
+    json_data["RegistrantContact"]["PhoneNumber"] = ""
+    json_data["RegistrantContact"]["State"] = ""
+    json_data["RegistrantContact"]["CountryCode"] = ""
+    json_data["RegistrantContact"]["ZipCode"] = ""
+    json_data["RegistrantContact"]["Email"] = ""
+    # json_data["RegistrantContact"]["FAX"] = ""
+
+    json_data["TechContact"]["FirstName"] = ""
+    json_data["TechContact"]["LastName"] = ""
+    json_data["TechContact"]["ContactType"] = ""
+    json_data["TechContact"]["OrganizationName"] = ""
+    json_data["TechContact"]["AddressLine1"] = ""
+    json_data["TechContact"]["AddressLine2"] = ""
+    json_data["TechContact"]["City"] = ""
+    json_data["TechContact"]["PhoneNumber"] = ""
+    json_data["TechContact"]["State"] = ""
+    json_data["TechContact"]["CountryCode"] = ""
+    json_data["TechContact"]["ZipCode"] = ""
+    json_data["TechContact"]["Email"] = ""
+    # json_data["TechContact"]["FAX"] = ""
+    return json_data
+
+def create_record_set_json():
+    json_data["Changes"]["Changes"][0]["Action"] = "UPSERT"
+    subName = subDomainName+"."+domainName
+    print("subDomainName is :",subName)
+    json_data["Changes"]["Changes"][0]["ResourceRecordSet"]["Name"] = subName
+    json_data["Changes"]["Changes"][0]["ResourceRecordSet"]["TTL"] = 60
+    json_data["Changes"]["Changes"][0]["ResourceRecordSet"]["ResourceRecords"][0]["Value"] = IpAddress
+    return json_data
+
+def delete_record_set_json():
+    json_data["Changes"]["Changes"][0]["Action"] = "DELETE"
+    subName = subDomainName+"."+domainName
+    print("subDomainName is :", subName)
+    json_data["Changes"]["Changes"][0]["ResourceRecordSet"]["Name"] = subName
+    json_data["Changes"]["Changes"][0]["ResourceRecordSet"]["TTL"] = 60
+    json_data["Changes"]["Changes"][0]["ResourceRecordSet"]["ResourceRecords"][0]["Value"] = IpAddress
+    return json_data
+
+def create_healthcheck():
+    json_data["HealthCheckConfig"]["IPAddress"]= str(IpAddress)
+    print(type(healthCheckport))
+    json_data["HealthCheckConfig"]["Port"]= int(healthCheckport)
+    json_data["HealthCheckConfig"]["Type"]= "HTTP"
+    json_data["HealthCheckConfig"]["FullyQualifiedDomainName"]=subDomainName+"."+domainName
+    json_data["HealthCheckConfig"]["ResourcePath"]= healthCheckpath
+    # json_data["HealthCheckConfig"]["ResourcePath"] = "/ui/auth/login",
+    json_data["HealthCheckConfig"]["RequestInterval"]= 30
+    json_data["HealthCheckConfig"]["FailureThreshold"]= 5
+    return json_data
---
+++ Content/Networking/DNS/Azure/README.md
@@ -0,0 +1,69 @@
+## Azure DNS zones  
+
+# Introduction
+    Azure DNS is a hosting service for DNS domains that provides name resolution by using Microsoft Azure infrastructure. By hosting your domains in Azure, you can manage your DNS records
+    to your website or web application
+
+Please refer the below link for more details.
+For your reference : https://docs.microsoft.com/en-us/azure/dns/dns-overview
+# Pre-Requisites
+ CloudCenter
+   - CloudCenter 5.x.x and above
+   - Knowledge on how to use Workload Manager
+
+Before you start with service import, Install Docker by following the steps provided here, on any linux based client machine.
+
+NOTE : You can skip the above step, if Docker Client is already installed and running in your machine.
+
+- You can check , if docker is installed , by running "docker -v"
+- You can check , if docker is running , by executing the command "systemctl status docker"
+- Importing the service
+- Step 1 : Download the service import utility file from here, and save the file on to your linux machine.
+
+    wget command may not be installed. Need to execute "yum install wget -y" in case of centos7.
+
+        Example:  wget https://github.com/datacenter/cloudcentersuite/raw/master/Content/Scripts/ServiceImportMaster.sh
+    After downloading ServiceImportMaster.sh, provide file permissions by executing "chmod 755 ServiceImportMaster.sh".
+
+- Step 2 : Execute the script from Step 1 using the following command.
+
+    sh ServiceImportMaster.sh
+    Once the script is run, please follow the prompts to import the service or the corresponding application profile.
+
+PLEASE NOTE : You be prompted with location of service bundle zip and/or application bundle zip on client machine. The files must be copied on to the repository before proceeding to deploy.
+     - Service Zip file under <service_path>/<your_bundle_name>
+                
+         Example : http://<Your_REPO_Server_IP>/<service_path>/AzureDnsZones.zip 
+
+     - Application Zip file under <app_path>/<your_package_name>
+        
+         Example: http://<Your_REPO_Server_IP>/<app_path>/petclinic.war
+# Service Package Bundle
+    The Package Service bundle consists of the following files:
+
+Shell script:
+
+   - service: Initiates the python script to start integration.
+
+Python script:
+
+- install_setup.py: The script will check all mandatory parameters available and installs necessary - 
+- python packages also invokes external life cycle action.
+- azure_dns_management.py: script that invokes the api for Azure functions like create record set DNS configuration .
+- prerequiste_environments.py : Script will check required parameter for Azure DNS management
+- util.py: utility file
+- error_utils.py: A script that handles error functionalities
+# External Lifecycle Actions as below
+- External Action Bundle:  services/Azure_dns_zones.zip
+- External Lifecycle Actions:
+            Start:
+                Script from bundle: service start
+            Stop:
+                Script from bundle: service stop
+# Deployment Parameters:
+
+| Parameter Name | Type | Mandotory | Description | Allowed Value | Default Value | 
+| ---- |----| ----| ---- | ---- | ----|
+| DomainName |	String | Yes|	Mention Existing Registered Domain Name in Azure,If DomainName is not Please create a DomainName in Azure App Service Eg : <SampleDomainName.com>|
+|SubDomainName | Sting | Yes | Mention new  valid subdomain Eg : <SubDomainName>| | |
+|IpAddress |String |Yes| Mention IpAddress of your Webapplication or Application Ip
