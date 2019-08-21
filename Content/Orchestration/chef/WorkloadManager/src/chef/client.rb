55951deb29646427853bb7e0d39b7e24562259f7
Riyas <rrizwana@cisco.com>
2019-08-21 16:01:53 +0530

    App profile updated

--- Content/Orchestration/chef/WorkloadManager/src/chef/client.rb
+++ Content/Orchestration/chef/WorkloadManager/src/chef/client.rb
@@ -3,6 +3,7 @@ current_dir = File.dirname(__FILE__)
 log_location     STDOUT
 chef_server_url  "https://%CHEF_HOSTNAME%/organizations/%CHEF_ORGANIZATION%"
 validation_client_name "%VALIDATION_CLIENT%"
-validation_key "#{current_dir}/%VALIDATION_CLIENT%.pem"
+validation_key "/etc/chef/%VALIDATION_CLIENT%.pem"
 node_name "%NODE_NAME%"
 trusted_certs_dir "/etc/chef/trusted_certs"
+ssl_verify_mode :verify_none
